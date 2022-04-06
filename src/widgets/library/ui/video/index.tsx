6cce5d232b113be6736ffb1d0593d653f55de593
xalvaine <xalvaine@gmail.com>
2022-04-06 03:05:39 +0300

    [feat] Add video

--- src/widgets/library/ui/video/index.tsx
+++ src/widgets/library/ui/video/index.tsx
@@ -1,14 +1,41 @@
 import styles from './video.module.scss'
 import Button from './assets/button.svg'
+import { useRef, useState } from 'react'
+import classNames from 'classnames'
 
 export const Video = () => {
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handleToggle = async () => {
+    const video = videoRef.current
+
+    if (!video) {
+      return
+    }
+
+    if (video.paused) {
+      setIsPlaying(true)
+      await video.play()
+    } else {
+      setIsPlaying(false)
+      video.pause()
+    }
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.video}>
-        <button className={styles.button}>
-          <Button />
-        </button>
-      </div>
+    <div className={styles.wrapper} onClick={handleToggle}>
+      <video
+        ref={videoRef}
+        src='/all_videos_05.mp4'
+        controls={false}
+        className={styles.video}
+      />
+      <button
+        className={classNames(isPlaying ? styles.buttonHidden : styles.button)}
+      >
+        <Button />
+      </button>
     </div>
   )
 }
