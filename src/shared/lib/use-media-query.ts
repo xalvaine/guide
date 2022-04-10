92f15a5df9a3a35e105da397d8cbdee20faf8362
xalvaine <xalvaine@gmail.com>
2022-04-10 22:59:43 +0300

    [fix] Increase display size in media trigger

--- src/shared/lib/use-media-query.ts
+++ src/shared/lib/use-media-query.ts
@@ -25,7 +25,7 @@ const useMediaQuery = (query: string) => {
 export { useMediaQuery }
 
 export const useIsMobile = () => {
-  const { matches, rendered } = useMediaQuery(`(max-width: 1023px)`)
+  const { matches, rendered } = useMediaQuery(`(max-width: 1200px)`)
 
   return { isMobile: matches, rendered }
 }
