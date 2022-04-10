3acd6aba772c7e650f12db62d53896b0d88d2bdf
xalvaine <xalvaine@gmail.com>
2022-04-10 20:57:34 +0300

    [feat] Add grid mobile page

--- src/widgets/identity-grid/index.tsx
+++ src/widgets/identity-grid/index.tsx
@@ -18,13 +18,31 @@ import Example2 from './assets/example2.svg'
 import { BlockTitle } from 'entities/block-title/ui'
 import { PageToggle } from 'shared/ui/page-toggle'
 import { Routes } from 'shared/config'
+import { useIsMobile } from 'shared/lib'
+import DeformationM from './assets/deformation_m.svg'
+import GridM from './assets/grid_m.svg'
+import CreationM from './assets/creation_m.svg'
+import DoublingM from './assets/doubling_m.svg'
+import ExamplesM from './assets/examples_m.svg'
+import Usage1M from './assets/usage1_m.svg'
+import Usage2M from './assets/usage2_m.svg'
+import Usage3M from './assets/usage4_m.svg'
+import Usage4M from './assets/usage5_m.svg'
+import LimitsM from './assets/limits_ь.svg'
+import ExamplesM2 from './assets/examples_m_2.svg'
 
 export const IdentityGrid = () => {
+  const { isMobile, rendered } = useIsMobile()
+
+  if (!rendered) {
+    return null
+  }
+
   return (
     <>
       <Header title='Айдентика бренда' />
       <Section title='Модульная сетка'>
-        <Paragraph style={{ marginTop: 120 }}>
+        <Paragraph style={{ marginTop: isMobile ? 40 : 120 }}>
           Чтобы обеспечить системность подхода и в то же время возможность
           творчески адаптировать бренд к различным точкам контакта, строится
           модульная сетка и определяется площадь набора текста.
@@ -49,16 +67,33 @@ export const IdentityGrid = () => {
           <br />
           <strong>Максимально допустимая деформация ячейки </strong>
         </Paragraph>
-        <Relative>
-          <Deformation style={{ position: `absolute`, top: -50 }} />
-        </Relative>
+        {isMobile ? (
+          <>
+            <DeformationM style={{ marginTop: 25 }} />
+            <Paragraph style={{ marginTop: 10 }}>
+              Для адаптации модульной сетки к формату конкретного носителя
+              допускается изменение пропорций ячеек со стандартных 4:4 до 4:5.
+            </Paragraph>
+            <GridM />
+          </>
+        ) : (
+          <Relative>
+            <Deformation style={{ position: `absolute`, top: -50 }} />
+          </Relative>
+        )}
         <BlockTitle
           title='Создание модульной сетки'
-          style={{ marginTop: 1150 }}
+          style={{ marginTop: isMobile ? 40 : 1150 }}
         />
         <Relative>
-          <Creation style={{ marginLeft: -10 }} />
-          <Paragraph style={{ width: 640, marginTop: 18 }}>
+          {isMobile ? (
+            <CreationM style={{ marginLeft: -8 }} />
+          ) : (
+            <Creation style={{ marginLeft: -10 }} />
+          )}
+          <Paragraph
+            style={{ width: isMobile ? undefined : 640, marginTop: 18 }}
+          >
             1. Постройте стандартную ячейку
             <br />
             <br />
@@ -80,18 +115,36 @@ export const IdentityGrid = () => {
             она покрывала <br />
             всю площадь носителя.
           </Paragraph>
-          <Doubling style={{ position: `absolute`, top: 570, right: 380 }} />
+          {isMobile && (
+            <Paragraph style={{ marginTop: 17, marginBottom: 13 }}>
+              <b>Дублирование базового модуля</b>
+            </Paragraph>
+          )}
+          {isMobile ? (
+            <DoublingM />
+          ) : (
+            <Doubling style={{ position: `absolute`, top: 570, right: 380 }} />
+          )}
         </Relative>
-        <BlockTitle title='Примеры' style={{ marginTop: 150 }} />
-        <Examples1 style={{ marginLeft: -10 }} />
-        <Examples2 style={{ marginTop: 100, marginLeft: -10 }} />
+        <BlockTitle
+          title='Примеры'
+          style={{ marginTop: isMobile ? 40 : 150 }}
+        />
+        {isMobile ? (
+          <ExamplesM style={{ marginLeft: -8 }} />
+        ) : (
+          <>
+            <Examples1 style={{ marginLeft: -10 }} />
+            <Examples2 style={{ marginTop: 100, marginLeft: -10 }} />
+          </>
+        )}
         <BlockTitle
           title={
             <>
               Размещение простых фигур <br />в модулях с пропорциями 4:5
             </>
           }
-          style={{ marginTop: 160 }}
+          style={{ marginTop: isMobile ? 40 : 160 }}
         />
         <Paragraph style={{ marginTop: 18 }}>
           Если пропорции модульной сетки не позволяют разместить в ней модуль
@@ -102,7 +155,12 @@ export const IdentityGrid = () => {
           Треугольные и прямоугольные фигуры можно деформировать. Деформация
           круглых фигур не допускается.
         </Paragraph>
-        <Guides style={{ marginTop: 60, marginLeft: -55 }} />
+        <Guides
+          style={{
+            marginTop: isMobile ? 8 : 60,
+            marginLeft: isMobile ? undefined : -55,
+          }}
+        />
         <BlockTitle
           title={
             <>
@@ -110,7 +168,7 @@ export const IdentityGrid = () => {
               Базовый модуль
             </>
           }
-          style={{ marginTop: 120 }}
+          style={{ marginTop: isMobile ? 33 : 120 }}
         />
         <Paragraph>
           Hа первом этапе построения модульной системы СИБУР используются{' '}
@@ -125,7 +183,11 @@ export const IdentityGrid = () => {
           Базовые модули можно объединить, заполнив их одинаковым цветом (см.
           пример 2).
         </Paragraph>
-        <Usage1 style={{ marginTop: 50, marginLeft: -15 }} />
+        {isMobile ? (
+          <Usage1M style={{ marginTop: 11, marginLeft: -11 }} />
+        ) : (
+          <Usage1 style={{ marginTop: 50, marginLeft: -15 }} />
+        )}
         <BlockTitle
           title={
             <>
@@ -133,9 +195,9 @@ export const IdentityGrid = () => {
               Базовый модуль
             </>
           }
-          style={{ marginTop: 120 }}
+          style={{ marginTop: isMobile ? 40 : 120 }}
         />
-        <Paragraph style={{ width: 970 }}>
+        <Paragraph style={{ width: isMobile ? undefined : 970 }}>
           Второй шаг — <b>заполнение базовых модулей простыми модулями</b>{' '}
           паттерна СИБУРa.
           <br />
@@ -147,9 +209,16 @@ export const IdentityGrid = () => {
           При этом можно использовать различные сочетания цветов из цветовой
           палитры бренда.
         </Paragraph>
-        <Usage2 style={{ marginTop: 20, marginLeft: -15 }} />
-        <BlockTitle title='Ограничения' style={{ marginTop: 120 }} />
-        <Paragraph style={{ width: 674 }}>
+        {isMobile ? (
+          <Usage2M style={{ marginTop: 25, marginLeft: -11 }} />
+        ) : (
+          <Usage2 style={{ marginTop: 20, marginLeft: -15 }} />
+        )}
+        <BlockTitle
+          title='Ограничения'
+          style={{ marginTop: isMobile ? 40 : 120 }}
+        />
+        <Paragraph style={{ width: isMobile ? undefined : 674 }}>
           Не допускается сочетать в одном макете треугольники или полукруги
           разной ориентации.
           <br />
@@ -157,7 +226,11 @@ export const IdentityGrid = () => {
           Не допускается сочетать полукруг с треугольником или с другим
           полукругом.
         </Paragraph>
-        <Limits style={{ marginTop: 40 }} />
+        {isMobile ? (
+          <LimitsM style={{ marginTop: 18 }} />
+        ) : (
+          <Limits style={{ marginTop: 40 }} />
+        )}
         <BlockTitle
           title={
             <>
@@ -166,9 +239,9 @@ export const IdentityGrid = () => {
               Сложный модуль
             </>
           }
-          style={{ marginTop: 120 }}
+          style={{ marginTop: isMobile ? 24 : 120 }}
         />
-        <Paragraph style={{ width: 970 }}>
+        <Paragraph style={{ width: isMobile ? undefined : 970 }}>
           Закончив работу с простыми модулями паттерна, можно для динамичности
           добавить к макету некоторые сложные модули.
           <br />
@@ -185,7 +258,11 @@ export const IdentityGrid = () => {
           перегружайте ими макет. При этом можно использовать различные
           сочетания цветов из цветовой палитры бренда.
         </Paragraph>
-        <Usage3 style={{ marginTop: 20, marginLeft: -15 }} />
+        {isMobile ? (
+          <Usage3M style={{ marginTop: 11, marginLeft: -11 }} />
+        ) : (
+          <Usage3 style={{ marginTop: 20, marginLeft: -15 }} />
+        )}
         <BlockTitle
           title={
             <>
@@ -193,9 +270,9 @@ export const IdentityGrid = () => {
               Модули фотоизображений
             </>
           }
-          style={{ marginTop: 120 }}
+          style={{ marginTop: isMobile ? 40 : 120 }}
         />
-        <Paragraph style={{ width: 970 }}>
+        <Paragraph style={{ width: isMobile ? undefined : 970 }}>
           Некоторые из базовых и простых модулей можно заполнить фотографиями.
           Изображения можно разместить в любом из модулей; в пределах модуля их
           разрешается свободно масштабировать.
@@ -204,10 +281,23 @@ export const IdentityGrid = () => {
           Количество фотоизображений в одном макете не должно превышать трех. Не
           следует перегружать фотоизображениями общую композицию макета.
         </Paragraph>
-        <Usage4 style={{ marginTop: 20, marginLeft: -15 }} />
-        <BlockTitle title='Примеры использования' style={{ marginTop: 120 }} />
-        <Example1 style={{ marginTop: 40 }} />
-        <Example2 style={{ marginTop: 80, marginBottom: 250 }} />
+        {isMobile ? (
+          <Usage4M style={{ marginTop: 25, marginLeft: -15 }} />
+        ) : (
+          <Usage4 style={{ marginTop: 20, marginLeft: -15 }} />
+        )}
+        <BlockTitle
+          title='Примеры использования'
+          style={{ marginTop: isMobile ? 40 : 120 }}
+        />
+        {isMobile ? (
+          <ExamplesM2 />
+        ) : (
+          <>
+            <Example1 style={{ marginTop: 40 }} />
+            <Example2 style={{ marginTop: 80, marginBottom: 250 }} />
+          </>
+        )}
         <PageToggle
           prevLink={Routes.IdentityPatterns}
           prevTitle='Паттерны'
