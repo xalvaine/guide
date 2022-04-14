6881a988198e2e26387253cbaa3c8f0f07002e0d
xalvaine <xalvaine@gmail.com>
2022-04-14 20:08:56 +0300

    [feat] Add preview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { Routes } from 'shared/config'
 
-export const HomePage = () => {
+export default function Home () {
   const { replace } = useRouter()
 
   useEffect(() => {
